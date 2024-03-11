898dfac4ddb3ce1a10af7985c90af5b305a9d6a7
Xuan  Tuoi <tuoittx@ikong.vn>
2024-03-11 21:44:01 +0700

    feat: add api get user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -2,10 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+  res.status(200).json({
+    status: 'success',
+    message: 'api ok',
+  });
+});
+
+router.get('/api/users', (req, res, next) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'api users ok',
+  });
+});
 
 module.exports = router;
